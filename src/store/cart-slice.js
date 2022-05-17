adfa0ffa1ad03367e276faa7d70591e4139d45d5
Marc Medhat <marcmedhat6211@gmail.com>
2022-05-17 18:48:13 +0200

    adding redux to the project, and handling the ui slice and the add item to cart action in the cart slice

---
+++ src/store/cart-slice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+  totalQuantity: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        }); //DO NOT DO THIS IF YOU ARE USING ONLY REDUX NOT REDUX TOOLKIT
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice += newItem.price;
+      }
+    },
+    removeItemFromCart() {},
+  },
+});
